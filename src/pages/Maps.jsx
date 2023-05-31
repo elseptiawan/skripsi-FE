651b4774873a03719d63ba3520f6a97c296ebd52
elseptiawan <septiawanelshandi@gmail.com>
2023-05-31 22:25:16 +0700

    Integrasi with BE

--- src/pages/Maps.jsx
+++ src/pages/Maps.jsx
@@ -1,42 +1,37 @@
 import style from "../Style/maps.module.css";
 import "leaflet/dist/leaflet.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 
 
-import { Icon, divIcon, point } from "leaflet";
+import { Icon } from "leaflet";
 
 const customIcon = new Icon({
     iconUrl: require("../icons/marker-icon.png"),
     iconSize: [30, 30] // size of the icon
   });
 
-// markers
-const markers = [
-    {
-      geocode: [-6.884864540043645, 107.61351436542057],
-      nama: "McDonalds Simpang Dago",
-      alamat: "Dago, Coblong"
-    },
-    {
-      geocode: [-6.883745917496636, 107.61475139219755],
-      nama: "RM Pagi Sero",
-      alamat: "Dago, Coblong"
-    },
-    {
-      geocode: [-6.888782225238749, 107.61314860883988],
-      nama: "Mie Gacoan Dago",
-      alamat: "Dago, Coblong"
-    }
-  ];
-
   export default function Maps() {
+    const position = [-6.901681163172566, 107.62512693756112]
     const navigate = useNavigate();
     const handleClick = (event) => {
         navigate('login');
       };
+    const [restorans, setRestorans] = useState([]);
+
+    useEffect(() => {
+      getRestorans();
+    }, []);
+
+    const getRestorans = async () => {
+      const response = await axios.get("http://localhost:3000/restorans");
+      setRestorans(response.data.data);
+    };
+
     return (
       <div className={style.container}>
         <div className={style.map}>
@@ -45,14 +40,14 @@ const markers = [
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {markers.map((marker, index) => (
-          <Marker key={index} position={marker.geocode} icon={customIcon}>
-            <Popup>{marker.nama} <br/> {marker.alamat}</Popup>
+        {restorans.map((restoran, index) => (
+          <Marker key={index} position={[restoran.latitude, restoran.longtitude]} icon={customIcon}>
+            <Popup>{restoran.nama} <br/> {restoran.alamat}</Popup>
           </Marker>
         ))}
       </MapContainer>
       </div>
-        <div class={style.topleft}>
+        <div className={style.topleft}>
           <form>
             <select name = "category">
             <option>Semua Kategori</option>
