d85645379a7776395e1f6304ce26ef86be1195b2
elseptiawan <septiawanelshandi@gmail.com>
2023-06-02 07:49:31 +0700

    Login API Integration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 import style from "../Style/login.module.css";
 import logo from '../icons/logo-halal.png';
 import { useNavigate } from "react-router-dom";
+import { setLocalStorage } from "../scripts/localStorage";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Login = () => {
   const navigate = useNavigate();
-  const handleClick = (event) => {
-    navigate('../dashboard');
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const { data: res } = await axios.post("http://localhost:3000/users/login", {
+        email,
+        password,
+      });
+
+      console.log(res.response);
+
+      setLocalStorage("token", res.token);
+      axios.defaults.headers.common["Authorization"] = `Bearer ${res.token}`;
+      setError("");
+      navigate("/dashboard");
+    } catch ({ response }) {
+      console.log(response);
+      setError(response.data.message);
+    }
+  }
+  
   return (
     <div className={style.flex_container}>
 
@@ -20,12 +44,25 @@ const Login = () => {
         <div className={style.form_container}>
           <div className={style.form_login}>
             <h2>Login Admin</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
             <label>Email</label><br/>
-            <input className={style.login_input} type="email" placeholder="Masukkan Email" name="email" required/><br/>
+            <input 
+            className={style.login_input} 
+            type="email" 
+            placeholder="Masukkan Email" 
+            name="email" 
+            onChange={(e) => setEmail(e.target.value)}
+            required/><br/>
             <label>Password</label><br/>
-            <input className={style.login_input} type="password" placeholder="Masukkan Password" name="password" required/><br/>
-            <button className={style.login_button} onClick={event => handleClick(event)}>
+            <input 
+            className={style.login_input} 
+            type="password" 
+            placeholder="Masukkan Password" 
+            name="password"
+            onChange={(e) => setPassword(e.target.value)} 
+            required/><br/>
+            {error && <div className={`${style.alert} danger`}>{error}</div>}
+            <button className={style.login_button}>
               Login
             </button>
             </form>
