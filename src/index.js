4f8e8a4f9aed5faa4e48dbc65ca8429e9b3d6582
elseptiawan <septiawanelshandi@gmail.com>
2023-06-03 21:52:13 +0700

    add default base url

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 const token = localStorage.getItem('token')
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
 root.render(
