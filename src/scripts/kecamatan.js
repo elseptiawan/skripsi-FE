722b4e8bba50535710e51aeb0019f325deab92be
elseptiawan <septiawanelshandi@gmail.com>
2023-06-03 21:54:02 +0700

    define string kecamatan

---
+++ src/scripts/kecamatan.js
@@ -0,0 +1,35 @@
+const kecamatan = () => {
+    return ([
+        'Andir',
+        'Astana Anyar',
+        'Antapani',
+        'Arcamanik',
+        'Babakan Ciparay',
+        'Bandung Kidul',
+        'Bandung Kulon',
+        'Bandung Wetan',
+        'Batununggal',
+        'Bojongloa Kaler',
+        'Bojongloa Kidul',
+        'Buah Batu',
+        'Cibeunying Kaler',
+        'Cibeunying Kidul',
+        'Cibiru',
+        'Cicendo',
+        'Cinambo',
+        'Coblong',
+        'GedeBage',
+        'KiaraCondong',
+        'Lengkong',
+        'MandalaJati',
+        'Panyileukan',
+        'Rancasari',
+        'Regol',
+        'Sukajadi',
+        'Sukasari',
+        'Sumur Bandung',
+        'Ujung Berung'
+    ])
+}
+
+export default kecamatan
